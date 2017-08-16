bff476e8862a02b7f47144b91e50594959be783d
vidhyakrish <vidhyaakrish@gmail.com>
2017-08-16 13:42:51 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-
 import App from './App';
+import './styles/index.css';
+import './styles/layout.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
